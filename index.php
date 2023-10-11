3e62074ec516d4ed76258b19f7a1e52416811bfd
João Henrique <santos.joao@crmsolucoes.com.br>
2023-10-11 11:12:52 -0300

    2 - Organizando pastas e dependencias

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de tarefas 2.0</title>
+    <link rel="stylesheet" href="src/css/style.min.css">
+</head>
+<body>
 
-require 'vendor/autoload.php';
+    <?php
 
-use Lista\Class\User;
+    require 'vendor/autoload.php';
 
-?>
+    use Lista\Class\User;
+
+    ?>
+
+    <script src="./node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+
+</body>
+</html>
