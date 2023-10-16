7966f89c07bacce99dd1827b76d4e77584a16a19
João Henrique <santos.joao@crmsolucoes.com.br>
2023-10-16 10:28:00 -0300

    Ajuste

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.closeModal').addEventListener('click', () => {
+
+    let modalMessage = document.querySelector('.card-message');
+
+    modalMessage.style.display = "none";
+
+});
